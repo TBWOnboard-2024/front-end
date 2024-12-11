1165ee3adb9f1487f22893157efca56a52d3b994
fac3m4n <akerimberdi@gmail.com>
2024-12-11 08:12:09 +0800

    some updates

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -11,7 +11,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
       <body>
-        <ThemeProvider enableSystem>
+        <ThemeProvider defaultTheme="light">
           <ScaffoldEthAppWithProviders>{children}</ScaffoldEthAppWithProviders>
           <Footer />
         </ThemeProvider>
